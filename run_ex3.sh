5d18260cc55a5aaea230fa38e224c5350df8c874
rore3789 <93150129+rore3789@users.noreply.github.com>
2023-09-06 14:09:19 -0600

    Update run_ex3.sh

--- run_ex3.sh
+++ run_ex3.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 THREADS=10
-EXPNUM=3
-REFINEMENTS=3
+EXPNUM=6
+REFINEMENTS=2
 export JULIA_NUM_THREADS=$THREADS
-#python3 setup_nn_bounds.py $THREADS $EXPNUM
+python3 setup_nn_bounds.py $THREADS $EXPNUM
 julia DeepKernelSynthesis.jl $THREADS $EXPNUM $REFINEMENTS
